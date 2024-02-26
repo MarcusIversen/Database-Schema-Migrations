71b9cc030bfb4284bd8fc3ea2a6e4dce13c0a223
Andylaa10 <andylaa100@gmail.com>
2024-02-26 13:11:00 +0100

    Fixed syntax in v0 and v1 migrations

--- migrations/V0_26022024__create_product_table.sql
+++ migrations/V0_26022024__create_product_table.sql
@@ -1,13 +1,15 @@
+-- Create eCommerceDB database
 CREATE
-Database eCommerceDB;
+DATABASE eCommerceDB;
 GO
 
 USE eCommerceDB;
 GO
 
+-- Create Product table
 CREATE TABLE Product
 (
-    Id    INT PRIMARY KEY IDENTITY(1,1),
-    Name  NVARCHAR(255) NOT NULL,
-    Price DECIMAL(10, 2) NOT NULL,
+    Id    INT PRIMARY KEY IDENTITY(1,1), -- Primary key for Product table
+    Name  NVARCHAR(255) NOT NULL,        -- Name of the product
+    Price DECIMAL(10, 2) NOT NULL        -- Price of the product
 );
