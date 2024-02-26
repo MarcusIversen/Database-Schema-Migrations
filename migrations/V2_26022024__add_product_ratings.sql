52397a143117e5f3527d44cc03b113b06c476ef1
Andylaa10 <andylaa100@gmail.com>
2024-02-26 13:16:45 +0100

    Update V2_26022024__add_product_ratings.sql

--- migrations/V2_26022024__add_product_ratings.sql
+++ migrations/V2_26022024__add_product_ratings.sql
@@ -1,7 +1,20 @@
-CREATE TABLE ProductRating(
+USE eCommerceDB;
+GO
+
+-- Create Product rating table
+CREATE TABLE ProductRating
+(
     Id INT PRIMARY KEY IDENTITY(1,1),
-    Rating DOUBLE CHECK (Rating BETWEEN 1 AND 10),
+    Rating FLOAT,
+    ProductId INT
 );
 
+-- Add CHECK constraint to ensure Rating is between 1 and 10
 ALTER TABLE ProductRating
-ADD FOREIGN KEY (ProductId ) REFERENCES Product (Id);
+    ADD CONSTRAINT CHK_RatingRange
+        CHECK (Rating BETWEEN 1 AND 10);
+
+-- Add foreign key constraint to link ProductRating and Product tables
+ALTER TABLE ProductRating
+    ADD CONSTRAINT FK_Product_ProductRating
+        FOREIGN KEY (ProductId) REFERENCES Product(Id);
