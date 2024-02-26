05d3a652a774f15b46e756dab8b939183fd0897e
Andylaa10 <andylaa100@gmail.com>
2024-02-26 12:59:48 +0100

    fix first migration v0

--- migrations/V0_26022024__create_product_table.sql
+++ migrations/V0_26022024__create_product_table.sql
@@ -1,7 +1,13 @@
-CREATE Database IF NOT EXISTS `eCommerceDB`;
-       
-CREATE TABLE Product(
-    Id INT AUTO_INCREMENT PRIMARY KEY,
-    Name NVARCHAR(255) NOT NULL,
-    Price DECIMAL(10,2) NOT NULL,
+CREATE
+Database eCommerceDB;
+GO
+
+USE eCommerceDB;
+GO
+
+CREATE TABLE Product
+(
+    Id    INT PRIMARY KEY IDENTITY(1,1),
+    Name  NVARCHAR(255) NOT NULL,
+    Price DECIMAL(10, 2) NOT NULL,
 );
